38bad12c3355cabf9a3d7ff59a14e9bd02c83a7a
burnt_cookie <dkcheetah12@gmail.com>
2026-09-15 21:28:23 -0600

    Bug Fixes
    Fixed an issue where any enemy spawned would be in the same spot as the last

--- map.gd
+++ map.gd
@@ -1,7 +1,9 @@
+#Map.gd
 extends Node
 
 @export var Map1: PackedScene
 @export var Map2: PackedScene
+@onready var timer: Timer = $Timer
 
 
 var current_map = Map1
@@ -11,14 +13,15 @@ func _ready() -> void:
 	var Map_Instance = Map2.instantiate()
 	current_map = Map_Instance
 	add_child(Map_Instance)
-
+	timer.timeout.connect(Map_Instance._on_timer_timeout)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	print(timer.time_left)
 
 func set_map(map_choice: PackedScene):
 	current_map.queue_free()
 	var Map_Instance = map_choice.instantiate()
 	current_map = Map_Instance
 	add_child(Map_Instance)
+	timer.timeout.connect(Map_Instance._on_timer_timeout)
--- map_1.gd
+++ map_1.gd
@@ -3,17 +3,22 @@ extends Node2D
 
 @export var enemies: Array[PackedScene]
 @onready var path_node = find_child("Path2D", true, false)
-@onready var path_follow = path_node.find_child("PathFollow2D", true, false)
-
+@onready var active_followers: Array[PathFollow2D] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_enemy(enemies[0])
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	path_follow.progress += 0.5
+func _physics_process(delta: float) -> void:
+	for follower in active_followers:
+		follower.progress += 0.5
+	
 
 func spawn_enemy(enemy_choice: PackedScene):
+	var follower := PathFollow2D.new()
+	path_node.add_child(follower)
 	var enemy_instance = enemy_choice.instantiate()
-	path.add_child(enemy_instance)
+	follower.add_child(enemy_instance)
+	active_followers.append(follower)
+	
+func _on_timer_timeout():
+	spawn_enemy(enemies[0])
